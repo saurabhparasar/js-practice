d05aa7e125e7eda1dc5ec76ccd4f76583b8809ce
saurabh parasar <saurabhparasar3010@gmail.com>
2021-06-07 02:51:59 +0530

    add

--- assignmments.js
+++ assignmments.js
@@ -79,3 +79,25 @@ if (population < 50 && language === "english" && !isIsland) {
 } else {
   console.log(`${country} does not meet your criteria`);
 }
+
+// ................................The switch Statement
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("4th place");
+    break;
+  case "arabic":
+    console.log("5th place");
+    break;
+  default:
+    console.log("Great language too :D");
+}
